6bf91a37aef398bfaeba55ca3bbd297045f6af93
leon-h-a <lekileo@gmail.com>
2025-11-08 22:11:13 +0100

    refactor: move non-code to data dir

--- src/page2rss/__init__.py
+++ src/page2rss/__init__.py
@@ -8,9 +8,11 @@ log_format = logging.Formatter(
 )
 
 PROJECT_ROOT: Path = Path(os.path.dirname(os.path.abspath(__file__)))
-INPUT_DIR: Path = PROJECT_ROOT / "pages"
-OUTPUT_DIR: Path = PROJECT_ROOT / "xml_output"
+PAGE_LIST: Path = PROJECT_ROOT / "data/page_list.json"
+INPUT_DIR: Path = PROJECT_ROOT / "data/pages"
+OUTPUT_DIR: Path = PROJECT_ROOT / "data/xml_output"
 
 TEST_ROOT: Path = Path(os.path.dirname(os.path.abspath(__file__))).parent.parent / "test/test_page2rss"
-TEST_INPUT_DIR: Path = TEST_ROOT / "pages"
-TEST_OUTPUT_DIR: Path = TEST_ROOT / "xml_output"
+TEST_PAGE_LIST: Path = TEST_ROOT / "data/page_list.json"
+TEST_INPUT_DIR: Path = TEST_ROOT / "data/pages"
+TEST_OUTPUT_DIR: Path = TEST_ROOT / "data/xml_output"
